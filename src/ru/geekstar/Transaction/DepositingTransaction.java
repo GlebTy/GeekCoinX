2ce6adf2c8fdae9e8a4b8cd2838b15f7f0cd14cf
Gleb Tyurin <gopocatgop@gmail.com>
2024-02-24 14:50:15 +0300

    отображения имени отправителя при переводе на чужую карту

--- src/ru/geekstar/Transaction/DepositingTransaction.java
+++ src/ru/geekstar/Transaction/DepositingTransaction.java
@@ -1,8 +1,42 @@
 package ru.geekstar.Transaction;
 
+import ru.geekstar.PhysicalPerson.PhysicalPerson;
+
 public class DepositingTransaction extends Transaction {
 
 
+    @Override
+    public String getSender() {
+
+        String sender = "";
+
+        // если списание происходит с карты
+        if(getFromCard() != null){
+
+            //если мы пополняем чужую карту или счет, то добавляем имя отправителя
+            if ((getToCard() != null && !getFromCard().getCardHolder().isClientCard(getToCard())) || (getToAccount() != null && !getFromCard().getCardHolder().isClientAccount(getToAccount()))) {
+
+                PhysicalPerson cardHolder = getFromCard().getCardHolder().getPhysicalPerson();
+                sender = cardHolder.getFirstName() + " " + cardHolder.getLastName().substring(0,1);
+            }
+
+        }
+        // если списание происходит со счета
+        if(getFromAccount() != null){
+
+            //если мы пополняем чужую карту или счет, то добавляем имя отправителя
+            if ((getToCard() != null && !getFromAccount().getAccountHolder().isClientCard(getToCard())) || (getToAccount() != null && !getFromAccount().getAccountHolder().isClientAccount(getToAccount()))) {
+
+                PhysicalPerson AccountHolder = getFromAccount().getAccountHolder().getPhysicalPerson();
+                sender = AccountHolder.getFirstName() + " " + AccountHolder.getLastName().substring(0,1);
+            }
+
+            if (!sender.isEmpty()) {
+                sender = super.getSender() + " от " + sender + ".";
+            }
+        }
+        return sender;
+    }
     @Override
     public String getStringTransaction() {
 
